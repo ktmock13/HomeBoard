1505f82739ed4ef34235fac997c9b75d57b26e0d
Keith Mock <kmock@apple.com>
2019-04-04 14:37:59 -0700

    feat(subs): hasura and postgres live

--- server/graphql/resolvers/index.js
+++
@@ -1,7 +0,0 @@
-import { mergeResolvers } from "merge-graphql-schemas";
-
-import FinEvent from "./FinEvent/";
-
-const resolvers = [FinEvent];
-
-export default mergeResolvers(resolvers);
--- server/graphql/types/index.js
+++
@@ -1,7 +0,0 @@
-import { mergeTypes } from "merge-graphql-schemas";
-
-import FinEvent from "./FinEvent/";
-
-const typeDefs = [FinEvent];
-
-export default mergeTypes(typeDefs, { all: true });
